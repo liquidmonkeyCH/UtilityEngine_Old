101884ecf006372a17c5963af1e074213ca4fcdb
hourui <47287984+liquidmonkeyCH@users.noreply.github.com>
2019-08-22 14:06:52 +0800

    1

---
+++ UtilityEngine/include/application.hpp
@@ -0,0 +1,65 @@
+/**
+* @file application.hpp
+*
+* @author Hourui (liquidmonkey)
+**/
+
+#ifndef __APPLICATION_HPP__
+#define __APPLICATION_HPP__
+
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <map>
+#include <vector>
+
+namespace Utility
+{
+////////////////////////////////////////////////////////////////////////////////
+namespace main
+{
+////////////////////////////////////////////////////////////////////////////////
+class application
+{
+private:
+	class controler
+	{
+	public:
+		controler(void) = default;
+		~controler(void) = default;
+		void run(void);
+		void stop(void);
+	private:
+		std::mutex m_mutex;
+		std::condition_variable	m_cv;
+	};
+public:
+	application(void) = default;
+	~application(void) = default;
+	using param_list = std::vector<std::string>;
+	using param_map = std::map<const char*,int>;
+	friend void on_signal(int n);
+public:
+	bool Start(int param_num, char* params[]);
+	void Run(void);
+protected:
+	size_t get_param_num(void);
+	const char* get_param(size_t n);
+	int get_param_pos(const char* param);
+protected:
+	virtual bool OnStart(void) = 0;
+	virtual void OnStop(void) = 0;
+private:
+	void daemon(void);
+	void setsignal(void);
+	static controler* get_controler(void);
+private:
+	param_list				m_param_list;
+	param_map				m_param_map;
+};
+////////////////////////////////////////////////////////////////////////////////
+}
+////////////////////////////////////////////////////////////////////////////////
+} //namespace Utility
+#endif //__APPLICATION_HPP__
+
---
+++ UtilityEngine/src/application.cpp
@@ -0,0 +1,147 @@
+/**
+* @file application.cpp
+*
+* @author Hourui (liquidmonkey)
+**/
+
+#include "application.hpp"
+#include "logger.hpp"
+
+#include <iostream>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
+namespace Utility
+{
+////////////////////////////////////////////////////////////////////////////////
+namespace main
+{
+////////////////////////////////////////////////////////////////////////////////
+void
+application::controler::run(void)
+{
+	std::unique_lock<std::mutex> lock(m_mutex);
+	m_cv.wait(lock);
+}
+////////////////////////////////////////////////////////////////////////////////
+void
+application::controler::stop(void)
+{
+	m_cv.notify_all();
+}
+////////////////////////////////////////////////////////////////////////////////
+application::controler*
+application::get_controler(void)
+{
+	static controler m_controler;
+	return &m_controler;
+}
+////////////////////////////////////////////////////////////////////////////////
+bool
+application::Start(int param_num, char* params[])
+{
+	bool bDaemon = false;
+	for (int i = 0; i < param_num; ++i)
+	{
+		if (strcmp(params[i], "-d") == 0 || strcmp(params[i], "-daemon") == 0)
+			bDaemon = true;
+
+		m_param_list.push_back(params[i]);
+		m_param_map.emplace(m_param_list.back().c_str(),i);
+	}
+
+	if (bDaemon)
+		daemon();
+
+	setsignal();
+	return OnStart();
+}
+////////////////////////////////////////////////////////////////////////////////
+void
+application::Run(void)
+{
+	Clog::info("Application starting!");
+	get_controler()->run();
+	OnStop();
+}
+////////////////////////////////////////////////////////////////////////////////
+void
+application::daemon(void)
+{
+#ifndef _WIN32
+	int pid = fork();
+	if (pid != 0)
+	{
+		// parent exit
+		_exit(-1);
+	}
+	// create a new session
+	setsid();
+
+	fclose(stdin);
+	fclose(stdout);
+	fclose(stderr);
+#endif // !_WIN32
+}
+////////////////////////////////////////////////////////////////////////////////
+void on_signal(int n)
+{
+	if (n == SIGINT || n == SIGTERM)
+	{
+		application::get_controler()->stop();
+		return;
+	}
+}
+////////////////////////////////////////////////////////////////////////////////
+void
+application::setsignal(void)
+{
+#ifndef _WIN32
+	signal(SIGHUP, SIG_IGN);
+	signal(SIGPIPE, SIG_IGN);
+	struct sigaction act;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_NODEFER | SA_ONSTACK | SA_RESETHAND;
+	act.sa_handler = on_signal;
+	sigaction(SIGINT, &act, NULL);
+	sigaction(SIGTERM, &act, NULL);
+#else
+	signal(SIGINT, on_signal);
+	signal(SIGTERM, on_signal);
+#endif // !_WIN32
+}
+////////////////////////////////////////////////////////////////////////////////
+size_t
+application::get_param_num(void)
+{
+	return m_param_list.size();
+}
+////////////////////////////////////////////////////////////////////////////////
+const char*
+application::get_param(size_t n)
+{
+	if (n >= m_param_list.size())
+		return nullptr;
+
+	return m_param_list[n].c_str();
+}
+////////////////////////////////////////////////////////////////////////////////
+int
+application::get_param_pos(const char* param)
+{
+	auto it = m_param_map.find(param);
+	if (it != m_param_map.end())
+		return it->second;
+
+	return -1;
+}
+////////////////////////////////////////////////////////////////////////////////
+}// namespace main
+////////////////////////////////////////////////////////////////////////////////
+} //namespace Utility
